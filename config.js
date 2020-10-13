493c3be78a03217e096820197b97e2b15ad82b36
MohamadKerdii <72776232+MohamadKerdii@users.noreply.github.com>
2020-10-13 06:38:14 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  TOKEN: "NzY1NDE2NDc3NjY1Nzg3OTA0.X4UfpQ.xAPmtfjgDwnRLIvDYY-BcPIIKu4",
+  YT_API_KEY: "",
+  prefix: "!", //بريفيكس او الامر حقك
+  devs: ["538105848669405234"] //ايدي حقك
+};
+// التوكين حقك
